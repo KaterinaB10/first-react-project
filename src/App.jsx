042938c2a1b7a416b4f2d967f7324825d5aacc24
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-03-05 11:14:54 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import "./index.css";
 
+// function MyComponent() {
+// svg
+// }
+
+function GreetingName(properties) {
+  return <p>Hello {properties.name} </p>;
+}
+// I should put it in one line
+
+// function Greeting() {
+//   const name = "Kate";
+//   return `Hello, ${name}`;
+// }
+
 function App() {
   // return <h1>Hello</h1>;
   return (
@@ -8,10 +22,19 @@ function App() {
       <header className="header">
         <h1>Hello</h1>
       </header>
-
       <main className="main">
         <h2>Hello</h2>
+        <GreetingName name="Kate" />
+        {/* <MyComponent /> */}
       </main>
+
+      <section>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </section>
 
       <footer className="footer">
         <h1>Hello</h1>
