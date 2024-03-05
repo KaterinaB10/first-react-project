38d0d6f794ab7c666d2b0708cdbf2bb7aa30ba03
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-03-05 09:56:37 +0100

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// const root = document.getElementById("root");
+// const app = ReactDOM.createRoot(root);
+// app.render(<h1>Hello</h1>);
+// why I don't see result locally?
+// why I can't add bracets (it should be  ((h1)))
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
